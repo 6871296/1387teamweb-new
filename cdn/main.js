e30e7ec64706f199ef9b5c44c41dc1e6fa559714
6871296 <guojiacheng1022@163.com>
2026-02-05 17:09:00 +0800

    init

---
+++ cdn/main.js
@@ -0,0 +1,39 @@
+const site='https://1387teamweb.github.io/nextweb'
+const cdnSite='https://1387-cdn.pythonanywhere.com'
+
+let username=''
+if(!window.localStorage.getItem('loginId')||window.localStorage.getItem('loginId')==null||window.localStorage.getItem('loginId')=='null')
+	username='游客'
+else{
+	let xhr=new XMLHttpRequest()
+	xhr.open('POST',cdnSite+'/internalApi/loginId2UserId')
+	xhr.send({
+		'loginId':window.localStorage.getItem('loginId')
+	})
+	if(xhr.status===200){
+		let currentUser=new user(JSON.parse(xhr.responseText)['userId'])
+	}else{
+		alert('登录过期，请重新登录！')
+		window.location.href=currentSite+'/login'
+	}
+}
+
+document.getElementById('year').textContent=new Date().getFullYear()
+
+document.getElementById('username').textContent=currentUser.name
+if(currentUser.home!==''){
+	document.getElementById('user-goto-home').href=currentUser.home
+	document.getElementById('user-goto-home-icon').src=cdnSite+'/user-social-links/user-has-home.ico'
+}
+if(currentUser.luogu!==''){
+	document.getElementById('user-goto-luogu').href=currentUser.luogu
+	document.getElementById('user-goto-luogu-icon').src=cdnSite+'/user-social-links/user-has-luogu.ico'
+}
+if(currentUser._40code!==''){
+	document.getElementById('user-goto-40code').href=currentUser._40code
+	document.getElementById('user-goto-40code-icon').src=cdnSite+'/user-social-links/user-has-40code.ico'
+}
+if(currentUser.github!==''){
+	document.getElementById('user-goto-github').href=currentUser.github
+	document.getElementById('user-goto-gihub-icon').src=cdnSite+'/user-social-links/user-has-github.ico'
+}
---
+++ cdn/userclass.js
@@ -0,0 +1,32 @@
+class User{
+	constructor(userId){
+		this.userId=userId
+		this.downloadAssets()
+	}
+	uploadAssets(){
+		let xhr = new XMLHttpRequest()
+		xhr.open('POST', cdnSite + '/internalApi/setUserInfo')
+		xhr.send({
+			'userId': userId,
+			'name':this.name,
+			'favicon':this.favicon
+		})
+		if (xhr.status === 200) {
+		} else console.error(`Failed to set user info of ${userId}!`)
+	}
+	downloadAssets(){
+		let xhr = new XMLHttpRequest()
+		xhr.open('POST', cdnSite + '/internalApi/getUserInfo')
+		xhr.send({
+			'userId': userId
+		})
+		if (xhr.status === 200) {
+			this.name = JSON.parse(xhr.responseText)['name']
+			this.favicon = JSON.parse(xhr.responseText)['favicon']
+			this.home=JSON.parse(xhr.responseText)['home']
+			this.luogu=JSON.parse(xhr.responseText)['luogu']
+			this._40code=JSON.parse(xhr.responseText)['40code']
+			this.github=JSON.parse(xhr.responseText)['github']
+		} else console.error(`Failed to get user info of ${userId}!`)
+	}
+}
